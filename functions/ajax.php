00222e08af30ee2ddc8a489db1522bf6b7397f6c
alexander vasilev <alexander.vasilev@protonmail.com>
2020-06-09 22:11:24 +0300

    replaced some double quoates and did a bit of code clean up

--- functions/ajax.php
+++ functions/ajax.php
@@ -1,17 +1,16 @@
 <?php
 //ajax for saving settings in admin panel
-add_action( "wp_ajax_dms_add_fields", "dms_ajax_update_fields" );
+add_action( 'wp_ajax_dms_add_fields', 'dms_ajax_update_fields' );
 function dms_ajax_update_fields() {
 
 	check_ajax_referer( 'dms_nonce_секюрити', 'security' );
 
-	global $wpdb;
 	$name            = '';
 	$options         = '';
 	$multisite       = 'none';
 	$sorting         = 'none';
 	$placeholder     = '';
-	$radiobuttons    = array( 'none', 'all', 'usersonly' );
+	$radio_buttons    = array( 'none', 'all', 'usersonly' );
 	$sorting_options = array( 'none', 'lastfirst', 'alphabetic' );
 
 	if ( array_key_exists( 'name', $_POST ) ) {
@@ -37,29 +36,29 @@ function dms_ajax_update_fields() {
 	//validate
 	if ( $name ) {
 		if ( preg_match( '/[\^<,\"@\/\{\}\(\)\*\$%\?=>:\|;#]+/i', $name ) ) {
-			_e( "This field can contain only letters.", 'dropdown-multisite-selector' );
+			_e( 'This field can contain only letters.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
 
-	if ( ! in_array( $multisite, $radiobuttons ) ) {
+	if ( ! in_array( $multisite, $radio_buttons, true ) ) {
 		_e( "Don't bug with the code, please!", 'dropdown-multisite-selector' );
 		die();
 	}
 
-	if ( ! in_array( $sorting, $sorting_options ) ) {
+	if ( ! in_array( $sorting, $sorting_options, true ) ) {
 		_e( "Don't bug with the code, please!", 'dropdown-multisite-selector' );
 		die();
 	}
 
 	if ( $options ) {
 		if ( ! is_array( $options ) ) {
-			_e( "Please make sure that you have entered all fields correctly.", 'dropdown-multisite-selector' );
+			_e( 'Please make sure that you have entered all fields correctly.', 'dropdown-multisite-selector' );
 			die();
 		}
 	} elseif ( ! $options && $multisite === 'none' ) {
-		_e( "Please enter a place tag name.", 'dropdown-multisite-selector' );
+		_e( 'Please enter a place tag name.', 'dropdown-multisite-selector' );
 		die();
 	}
 
@@ -72,39 +71,39 @@ function dms_ajax_update_fields() {
 
 	if ( get_option( 'dms_select_name' ) != $name ) {
 		if ( ! update_option( 'dms_select_name', $name ) ) {
-			_e( "Something went wrong with updating your settings.", 'dropdown-multisite-selector' );
+			_e( 'Something went wrong with updating your settings.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
-	if ( get_option( 'dms_options' ) != $options && $multisite === 'none' ) {
+	if ( $multisite === 'none' &&  get_option( 'dms_options' ) != $options  ) {
 		if ( ! update_option( 'dms_options', $options ) ) {
-			_e( "Something went wrong with updating your settings.", 'dropdown-multisite-selector' );
+			_e( 'Something went wrong with updating your settings.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
 	if ( get_option( 'dms_multisite' ) != $multisite ) {
 		if ( ! update_option( 'dms_multisite', $multisite ) ) {
-			_e( "Something went wrong with updating your settings.", 'dropdown-multisite-selector' );
+			_e( 'Something went wrong with updating your settings.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
 	if ( get_option( 'dms_sorting' ) != $sorting ) {
 		if ( ! update_option( 'dms_sorting', $sorting ) ) {
-			_e( "Something went wrong with updating your settings.", 'dropdown-multisite-selector' );
+			_e( 'Something went wrong with updating your settings.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
 	if ( get_option( 'dms_placeholder' ) != $placeholder ) {
 		if ( ! update_option( 'dms_placeholder', $placeholder ) ) {
-			_e( "Something went wrong with updating your settings.", 'dropdown-multisite-selector' );
+			_e( 'Something went wrong with updating your settings.', 'dropdown-multisite-selector' );
 			die();
 		}
 	}
 
 	echo true;
-	die();
+	wp_die();
 }
